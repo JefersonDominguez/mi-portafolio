3983b6f973915f39bcc7fe012af7c9d7a5723e63
JefersonDominguez <jeffersondominguez082@gmail.com>
2026-07-28 12:54:06 -0500

    projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,9 @@
+// src/components/Projects.jsx
+export default function Projects() {
+  return (
+    <section>
+      <h2>Mis Proyectos</h2>
+      <p>Aquí irá la lista de mis proyectos.</p>
+    </section>
+  );
+}
